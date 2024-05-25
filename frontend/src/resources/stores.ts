fc4f73c6a678a0f0a9485904e93f953aedbf3016
Julien W <cefadrom1@gmail.com>
2024-05-25 18:36:58 +0200

    feat: generic function to get value from store in ts

--- frontend/src/resources/stores.ts
+++ frontend/src/resources/stores.ts
@@ -1,23 +1,18 @@
-import { readable, writable } from 'svelte/store';
+import { type Readable, readable, type Writable, writable } from 'svelte/store';
 import mapboxgl from 'mapbox-gl';
 import Directions, { type DirectionsService } from '@mapbox/mapbox-sdk/services/directions';
 
-export const mapStore = writable<mapboxgl.Map>();
-
-export const getMap = () => new Promise<mapboxgl.Map>((resolve) => {
-    const unsubscribe = mapStore.subscribe(value => {
+const storeToValue = <T>(store: Writable<T> | Readable<T>): Promise<T> => new Promise<T>((resolve) => {
+    const unsubscribe = store.subscribe(value => {
         resolve(value);
     });
     unsubscribe();
 });
+
+export const mapStore = writable<mapboxgl.Map>();
+export const getMap = () => storeToValue(mapStore);
 
 export const directionsClient = readable<DirectionsService>(Directions({
     accessToken: import.meta.env.PUBLIC_MAPBOX_TOKEN,
 }));
-
-export const getDirectionsClient = () => new Promise<DirectionsService>((resolve) => {
-    const unsubscribe = directionsClient.subscribe(value => {
-        resolve(value);
-    });
-    unsubscribe();
-});
+export const getDirectionsClient = () => storeToValue(directionsClient);
