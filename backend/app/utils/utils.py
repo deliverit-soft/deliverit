142273167e8944f5d22377afc367a5e5ede70318
Julien W <50249422+julien-wff@users.noreply.github.com>
2024-05-31 10:23:09 +0200

    feat: bin packing endpoint

--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -1,4 +1,6 @@
 import unicodedata
+import json
+import numpy as np
 
 
 def to_ascii(s):
@@ -8,3 +10,24 @@ def to_ascii(s):
 
 def clamp(value, min_value, max_value):
     return max(min(value, max_value), min_value)
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def numpy_to_object(array):
+    """
+    Function to convert a numpy array to an object
+    :param array: Numpy array
+    :return: Object
+    """
+    return json.loads(json.dumps(array, cls=NumpyEncoder))
