b161d2d36b811e2d78569b0f510fa2bd93d4cf0e
Julien W <50249422+julien-wff@users.noreply.github.com>
2024-06-02 23:10:09 +0200

    feat: calculate and display real roads

--- frontend/src/helpers/utils.ts
+++ frontend/src/helpers/utils.ts
@@ -1,6 +1,8 @@
 export const randint = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
 export const range = (min: number, max: number) => Array.from({ length: randint(min, max) }, (_, i) => i + min);
 
+export const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 /**
  * Format a duration in milliseconds to a human-readable string.
  * @param duration
@@ -14,4 +16,30 @@ export function durationFormat(duration: number) {
         return `${seconds}s ${String(milliseconds).padStart(3, '0')}ms`;
     else
         return `${minutes}m ${String(seconds).padStart(2, '0')}s`;
+}
+
+
+/**
+ * Take a list of elements and return a 2D list, with sub-list having at most `size` elements
+ * @param elements
+ * @param size
+ */
+export function chunkify<T>(elements: T[], size: number): T[][] {
+    const chunks: T[][] = [];
+    for (let i = 0; i < elements.length; i += size)
+        chunks.push(elements.slice(i, i + size));
+    return chunks;
+}
+
+
+/**
+ * Same as regular `chunkify`, but with last element from previous chunk repeated in the next one
+ * @param elements
+ * @param size
+ */
+export function chunkifyPath<T>(elements: T[], size: number): T[][] {
+    const chunks: T[][] = [];
+    for (let i = 0; i < elements.length; i += size - 1)
+        chunks.push(elements.slice(i, i + size));
+    return chunks;
 }
